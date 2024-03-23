829492256db724ea3730e00ec62a76054afa27ee
Rada Baklarova <rea2@abv.bg>
2024-03-23 18:04:44 +0200

    Add project files.

---
+++ EyeStarReservationSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EyeStarReservationSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
